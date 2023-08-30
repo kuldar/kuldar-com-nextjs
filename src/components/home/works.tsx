bc1332d665628dbfbfac48edd1f40e7751124bbc
Kuldar <hi@kuldar.com>
2023-08-30 12:47:49 +0300

    fix?

--- src/components/home/works.tsx
+++ src/components/home/works.tsx
@@ -6,6 +6,7 @@ import { animate, transitions, variants, whileInView } from '@/utils/animations'
 import SectionTitle from '@/components/shared/section-title'
 import Illustrations from '@/components/svgs/illustrations'
 import Icons from '@/components/svgs/icons'
+import Link from 'next/link'
 
 export const works = [
   {
@@ -41,35 +42,38 @@ export default function Works() {
       {/* List of work */}
       <div className="mt-4 grid grid-cols-6 gap-4 xs:mt-6 xs:gap-8">
         {works.map((work) => (
-          <motion.a
+          <motion.div
             {...whileInView}
             whileInView="show"
             transition={transitions.default}
             variants={variants.fadeInDown}
             key={work.name}
-            href={work.link}
             className="group/card relative col-span-6 overflow-hidden rounded-2xl border-t border-gray-50/20 bg-gradient-to-br from-gray-1000 via-gray-800 to-gray-950 active:top-[1px] md:col-span-3 last:md:col-span-6 lg:col-span-2 last:lg:col-span-2"
           >
-            <work.illustration className="absolute text-gray-500/80" />
+            <Link href={work.link} className="h-full w-full">
+              <work.illustration className="absolute text-gray-500/80" />
 
-            {/* Content */}
-            <div className="relative flex h-full flex-col items-start bg-gray-950/50 p-8 transition-colors group-hover/card:bg-transparent">
-              <h2 className="text-5xl font-bold tracking-snug">{work.name}</h2>
+              {/* Content */}
+              <div className="relative flex h-full flex-col items-start bg-gray-950/50 p-8 transition-colors group-hover/card:bg-transparent">
+                <h2 className="text-5xl font-bold tracking-snug">
+                  {work.name}
+                </h2>
 
-              <div className="mb-10 mt-6 flex-1 space-y-6 pr-4 text-lg leading-normal text-gray-50">
-                {work.description}
+                <div className="mb-10 mt-6 flex-1 space-y-6 pr-4 text-lg leading-normal text-gray-50">
+                  {work.description}
+                </div>
+
+                <span className="inline-flex items-center space-x-2 rounded-lg border border-gray-500 bg-gray-900 py-2 pl-2.5 pr-2 transition-colors group-hover/card:border-gray-200">
+                  <div className="text-sm font-bold leading-none text-white">
+                    View more
+                  </div>
+                  <div className="flex h-[18px] w-[18px] items-center justify-center rounded-full border border-gray-200 transition-colors group-hover/card:border-green-400 group-hover/card:bg-green-400">
+                    <Icons.Arrow className="h-2 w-2 text-white transition-colors group-hover/card:text-gray-1000" />
+                  </div>
+                </span>
               </div>
-
-              <span className="inline-flex items-center space-x-2 rounded-lg border border-gray-500 bg-gray-900 py-2 pl-2.5 pr-2 transition-colors group-hover/card:border-gray-200">
-                <div className="text-sm font-bold leading-none text-white">
-                  View more
-                </div>
-                <div className="flex h-[18px] w-[18px] items-center justify-center rounded-full border border-gray-200 transition-colors group-hover/card:border-green-400 group-hover/card:bg-green-400">
-                  <Icons.Arrow className="h-2 w-2 text-white transition-colors group-hover/card:text-gray-1000" />
-                </div>
-              </span>
-            </div>
-          </motion.a>
+            </Link>
+          </motion.div>
         ))}
       </div>
     </div>
