bcd0f7400966f3a82de93688998af37baeae0ea0
Kuldar <hi@kuldar.com>
2023-09-01 14:04:16 +0300

    update

--- src/components/home/articles-placeholder.tsx
+++ src/components/home/articles-placeholder.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 import { transitions, variants, whileInView } from '@/utils/animations'
 
 // Articles placeholder
-export async function ArticlesPlaceholder() {
+export function ArticlesPlaceholder() {
   return (
     <div className="relative order-1 col-span-12 border-r border-gray-500 bg-gradient-to-br from-gray-700 via-gray-1000 to-gray-1000 lg:order-2 lg:col-span-7 xl:col-span-8">
       <div className="h-full border-l border-gray-500 bg-gray-1000/20 p-6 pr-0 transition-colors hover:bg-transparent xs:p-10 lg:border-l-0">
