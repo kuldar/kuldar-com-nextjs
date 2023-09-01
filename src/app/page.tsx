1a8eb7efce1cd381a2283acad41090b7eeb9bf54
Kuldar <hi@kuldar.com>
2023-09-01 14:01:46 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
       <div className="border-b border-gray-500">
         <div className="mx-auto grid max-w-8xl grid-cols-12 px-2 xs:px-6 sm:px-10">
           <Companies />
-          <ArticlesPlaceholder />
+          {/* <ArticlesPlaceholder /> */}
         </div>
 
         <div className="mx-auto grid max-w-8xl grid-cols-12 px-2 xs:px-6 sm:px-10">
