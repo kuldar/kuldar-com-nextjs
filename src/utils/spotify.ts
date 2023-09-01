29f347fb13eee577598a144a705f8a771348cb12
Kuldar <hi@kuldar.com>
2023-09-01 13:27:46 +0300

    update

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -45,7 +45,7 @@ export async function getCurrentlyPlaying() {
 
   const response = await fetch(
     'https://api.spotify.com/v1/me/player/currently-playing',
-    { headers: { Authorization: `Bearer ${access_token}` } }
+    { headers: { Authorization: `Bearer ${access_token}` }, cache: 'no-store' }
   )
 
   if (response.status === 204 || response.status > 400) {
