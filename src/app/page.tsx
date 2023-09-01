8e50ba8dd4e2ca17beba6f9525d9122e4190b9ff
Kuldar <hi@kuldar.com>
2023-09-01 13:53:26 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Experiments from '@/components/home/experiments'
 import Portrait from '@/components/home/portrait'
 import Companies from '@/components/home/companies'
 // import { ArticlesPlaceholder } from '@/components/home/articles-placeholder'
-// import Works from '@/components/home/works'
+import Works from '@/components/home/works'
 import Footer from '@/components/shared/footer'
 
 // Home page
@@ -30,7 +30,7 @@ export default async function Home() {
         </div>
 
         <div className="mx-auto grid max-w-8xl grid-cols-12 px-2 xs:px-6 sm:px-10">
-          {/* <Works /> */}
+          <Works />
         </div>
       </div>
 
